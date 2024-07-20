72f7f60e222ab8c0d344712ecd74ab0e46afd01d
Devansh Handa <106070614+DevanshHanda@users.noreply.github.com>
2024-07-20 15:10:04 +0530

    Time: 2925 ms (8.63%), Space: 19.5 MB (7.12%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,4 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return 2*sum(set(nums))-sum(nums)
-            
+        return [k for k in set(nums) if nums.count(k)==1][0]
