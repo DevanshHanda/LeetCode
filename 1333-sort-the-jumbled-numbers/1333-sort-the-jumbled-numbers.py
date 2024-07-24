5b2486f2143c9ed1c0c19250e42cc97515174c2b
Devansh Handa <106070614+DevanshHanda@users.noreply.github.com>
2024-07-24 23:54:54 +0530

    Time: 945 ms (80.98%), Space: 24.4 MB (67.93%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -1,15 +1,13 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         d={str(i):v for i,v in enumerate(mapping)}
-        # print(d)
         d[' ']=0
         k = len(str(max(nums)))
-        # print(k)
         nums = [ ' ' * (k-len(str(i))) + str(i) for i in nums ]
-        # print(nums)
+        
+        # Radix sort
         for i in range(k-1,-1,-1):
             nums.sort(key=lambda x: d[x[i]])
-            # print(nums)
-        
+           
         nums = [int(i) for i in nums]
         return nums
