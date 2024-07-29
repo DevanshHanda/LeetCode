7909bde1d55eaee21e6781778ed9f3540e9a5fb2
Devansh Handa <106070614+DevanshHanda@users.noreply.github.com>
2024-07-29 07:49:50 +0530

    Time: 27 ms (98.45%), Space: 16.6 MB (30.62%) - LeetHub

---
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/1689-detect-pattern-of-length-m-repeated-k-or-more-times.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        streak = 0
+        for i in range(len(arr)-m):
+            
+            if arr[i] == arr[i+m]:
+                streak = streak + 1  
+            else: 
+                streak = 0
+
+            if streak == (k-1)*m: 
+                return True
+
+        return False        
