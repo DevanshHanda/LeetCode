89856879fdcdae7e0aebb4ced04aef8c19f39dd8
Devansh Handa <106070614+DevanshHanda@users.noreply.github.com>
2024-07-31 23:05:06 +0530

    Time: 57 ms (25.42%), Space: 16.8 MB (97.18%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.py
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.py
@@ -1,27 +1,43 @@
-from typing import List
+# from typing import List
+
+# class Solution:
+#     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+#         return self.arrangeBooks(books, shelfWidth)
+
+#     def arrangeBooks(self, books: List[List[int]], maxShelfWidth: int) -> int:
+#         minHeights = [float('inf')] * (len(books) + 1)
+#         minHeights[0] = 0
+
+#         for bookIndex in range(1, len(books) + 1):
+#             currentShelfHeight = 0
+#             currentShelfWidth = 0
+
+#             for lastBook in range(bookIndex - 1, -1, -1):
+#                 currentBookThickness, currentBookHeight = books[lastBook]
+
+#                 if currentShelfWidth + currentBookThickness > maxShelfWidth:
+#                     break
+
+#                 currentShelfWidth += currentBookThickness
+#                 currentShelfHeight = max(currentShelfHeight, currentBookHeight)
+
+#                 currentArrangementHeight = minHeights[lastBook] + currentShelfHeight
+#                 minHeights[bookIndex] = min(minHeights[bookIndex], currentArrangementHeight)
+
+#         return minHeights[len(books)]
 
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        return self.arrangeBooks(books, shelfWidth)
-
-    def arrangeBooks(self, books: List[List[int]], maxShelfWidth: int) -> int:
-        minHeights = [float('inf')] * (len(books) + 1)
-        minHeights[0] = 0
-
-        for bookIndex in range(1, len(books) + 1):
-            currentShelfHeight = 0
-            currentShelfWidth = 0
-
-            for lastBook in range(bookIndex - 1, -1, -1):
-                currentBookThickness, currentBookHeight = books[lastBook]
-
-                if currentShelfWidth + currentBookThickness > maxShelfWidth:
+        n = len(books)
+        dp = [0] * (n+1)
+        dp[n-1] = books[n-1][1]
+        for i in range(n-2, -1, -1):
+            curr_w, curr_h = books[i]
+            dp[i] = dp[i+1] + curr_h
+            for j in range(i+1, n):
+                curr_h = max(curr_h, books[j][1])
+                curr_w += books[j][0]
+                if curr_w > shelfWidth:
                     break
-
-                currentShelfWidth += currentBookThickness
-                currentShelfHeight = max(currentShelfHeight, currentBookHeight)
-
-                currentArrangementHeight = minHeights[lastBook] + currentShelfHeight
-                minHeights[bookIndex] = min(minHeights[bookIndex], currentArrangementHeight)
-
-        return minHeights[len(books)]
+                dp[i] = min(dp[i], dp[j+1]+curr_h)
+        return dp[0]
