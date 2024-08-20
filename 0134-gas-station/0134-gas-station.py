aff5d030a734b8b524bad63d6283c0d798b5ba16
Devansh Handa <106070614+DevanshHanda@users.noreply.github.com>
2024-08-20 23:08:58 +0530

    Time: 942 ms (33.79%), Space: 22 MB (30.9%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,29 +1,51 @@
+# class Solution:
+#     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+#         tank = 0
+#         l=[]
+#         diff = []
+#         for i,v in enumerate(gas):
+#             tank+=v-cost[i]
+#             diff.append(v-cost[i])
+#             l.append(tank)
+#         # print(l)
+#         t = min(l)
+#         # print(t)
+#         l = [i-t for i in l]
+#         # print(min(l))
+#         l_temp=[]
+#         l_temp[:] = l[::-1]
+#         ind = (len(l)-l_temp.index(0))%len(gas)
+#         parse = l[ind:]+l[:ind]
+#         diff = diff[ind:]+diff[:ind]
+#         tank=0
+#         j=0
+#         for i in parse:
+#             # print(tank,i,diff[j])
+#             tank+=diff[j]
+#             j+=1
+#             if i > tank:
+#                 return -1
+
+#         return ind
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        tank = 0
-        l=[]
-        diff = []
-        for i,v in enumerate(gas):
-            tank+=v-cost[i]
-            diff.append(v-cost[i])
-            l.append(tank)
-        # print(l)
-        t = min(l)
-        # print(t)
-        l = [i-t for i in l]
-        # print(min(l))
-        l_temp=[]
-        l_temp[:] = l[::-1]
-        ind = (len(l)-l_temp.index(0))%len(gas)
-        parse = l[ind:]+l[:ind]
-        diff = diff[ind:]+diff[:ind]
-        tank=0
-        j=0
-        for i in parse:
-            # print(tank,i,diff[j])
-            tank+=diff[j]
-            j+=1
-            if i > tank:
-                return -1
+        #gas =  [2,3,4]
+        #cost = [3,4,3]
+        #total =-1,-1,1
+        #sum of gas>= sum cost array
+        sum_cost = sum(cost)
+        sum_gas = sum(gas)
+        # Check if it is possible to complete the journey
+        if sum_cost > sum_gas:
+            return -1
 
-        return ind
+        current_gas = 0
+        starting_index = 0
+
+        for i in range(len(gas)):
+            current_gas += gas[i] - cost[i]
+            if current_gas < 0:
+                current_gas = 0
+                starting_index = i + 1
+        return starting_index
+        
